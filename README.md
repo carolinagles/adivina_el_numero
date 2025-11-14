36b44a569942d97d3ad0910c9adfbb7a7a3d5ac3
María Carolina <carolinagles@gmail.com>
2025-11-14 00:20:45 -0700

    adivinanza

---
+++ README.md
@@ -0,0 +1,37 @@
+# Adivina el número 
+
+Mediante el desarrollo del siguiente **juego de adivinanza en Python** deberá utilizarse todo lo aprendido sobre lo básico de python, valorando la correcta decisión en el uso de los algoritmos.   
+
+La estructura del juego será la siguente:
+
+### Menú principal
+
+Al comenzar el juego y durante el desarrollo del mismo, las opciones que se muestran hasta elegir la opción salir son:
+
+1. **Partida modo solitario**
+   *(Modo solitario supone que el número a adivinar es generado aleatoriamente por el ordenador).*
+2. **Partida 2 Jugadores**
+   *(Primero se escribe un número a adivinar y luego un segundo jugador intenta adivinarlo).*
+3. **Estadística**
+   *(Mostrará los datos almacenados en el fichero Excel, donde se podrá observar el historial de partidas jugadas).*
+4. **Salir**
+
+#### Submenú de dificultad
+
+Tanto la opción 1 como la opción 2 tendrán un submenú para elegir la dificultad.     
+En ambos menús debe chequearse que la opción elegida es válida y avisar en caso contrario.    
+
+1. **Fácil** (20 intentos)
+2. **Medio** (12 intentos)
+3. **Difícil** (5 intentos)
+
+### Reglas del juego
+* El número a adivinar debe ser entre **1 y 1000**.
+* Si se adivina el número, se gana la partida y se vuelve al menú principal.
+* Si no se adivina, el programa debe indicar si el número buscado es **mayor o menor**.
+* Si se supera el número de intentos, se pierde y se vuelve al menú principal.
+
+### Registro de resultados
+
+* Al finalizar cada partida (ya sea ganada o perdida), se pide el **nombre del jugador**.
+* Se guarda esta información junto con el resultado de la partida en un **fichero Excel**.
---
+++ modulos/historial.py
@@ -0,0 +1,85 @@
+"""
+Historial de partidas
+
+- Mostrar en formato tabla la ultimas 20 partidas 
+- Mejores Jugadores en gráfico de barras
+- Gráfico de pastele para ganados/perdidos
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+import openpyxl
+from .configuraciones import estadisticas
+
+
+def historial():
+    while True:
+        print("\n1. Últimas 20 Partidas\n2. Mejores Jugadores\n3. Partidas Ganadas o Perdidas\n4. Regresar\n")
+        opcion=input("Selecciona una opción númerica: ")
+        
+        if opcion == '1':
+            ultimas_partidas()
+        elif opcion == '2':
+            mejores_jugadores()            
+        elif opcion == '3':
+            graf_partidas()
+        elif opcion == '4':
+            break
+        else:
+            print("Selecciona una opción entre 1-3: ")
+
+            
+# Últimas partidas            
+def ultimas_partidas():
+    print("\n   Últimas partidas")
+    display(pd.read_excel(estadisticas).tail(20))
+
+
+# Mejores jugadores
+def mejores_jugadores():
+    df = pd.read_excel(estadisticas)
+    jugadores_ganadas = df[df['Resultado'] == 'Ganado']['Nombre'].value_counts().plot.bar(
+        title="Mejores 3 Jugadores",
+        color="cornflowerblue",
+        edgecolor="white"
+    )
+    plt.ylabel("Victorias")
+    plt.xlabel('')
+    plt.xticks(rotation=45)
+    plt.show()
+
+
+# Gráficos 
+def graf_partidas():
+    df = pd.read_excel(estadisticas)
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 3))
+
+    # generales    
+    resultado_generales = df['Resultado'].value_counts().plot.pie(
+        ax=ax1,
+        title="Partidas Generales",
+        shadow=True,
+        colors=["mediumseagreen", "tomato"],
+        autopct=lambda p: f'{p * sum(df["Resultado"].value_counts()) / 100:.0f}'
+    )
+    ax1.set_ylabel('')
+    
+    # solitarios
+    resultado_solitarios = df[df['Modo']=="Solitario"]['Resultado'].value_counts().plot.pie(
+        ax=ax2,
+        title="Partidas Solitarias",
+        shadow=True,
+        colors=["mediumseagreen", "tomato"],
+        autopct=lambda p: f'{p * sum(df["Resultado"].value_counts()) / 100:.0f}'
+    )
+    ax2.set_ylabel('')
+
+    # dobles
+    resultado_dobles = df[df['Modo']=="Doble"]['Resultado'].value_counts().plot.pie(
+        ax=ax3,
+        title="Partidas Dobles",
+        shadow=True,
+        colors=["mediumseagreen", "tomato"],
+        autopct=lambda p: f'{p * sum(df["Resultado"].value_counts()) / 100:.0f}'
+    )
+    ax3.set_ylabel('')
+    plt.show()

10567d225434ac3ed0501b21a737cf9aa552d051
María Carolina <carolinagles@gmail.com>
2025-11-15 20:55:52 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+"""
+Adivina el numero (1-1000)
+
+- Dos modos de juego, individual o en pareja.
+- Tres niveles de dificultad.
+- Registro partidas. 
+- Visualización de resultados. 
+
+requierements: 
+pip install openpyxl
+pip install matplotlib
+"""
+
+from modulos.menu import menu
+
+if __name__ == "__main__":
+  menu()
